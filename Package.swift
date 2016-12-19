0433d8431d634cd3dd123e84c591347af1c68b42
Jonathan Guthrie <jono@perfect.org>
2016-12-19 16:24:44 -0500

    Change lib name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // Date: 2016-12-19 19:52:53 +0000
 import PackageDescription
 let package = Package(
-    name: "Perfect-Session-PostgreSQL",
+    name: "PerfectSessionPostgreSQL",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 0, minor: 0),
