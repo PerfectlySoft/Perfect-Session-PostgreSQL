5d9a3fc6cc7677097f76a3a775b8f3aeaf931da5
Jonathan Guthrie <jono@perfect.org>
2017-01-12 15:46:49 -0500

    update dep rev

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-19 19:52:53 +0000
+// Date: 2017-01-12 20:41:34 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectSessionPostgreSQL",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
     ]
 )
