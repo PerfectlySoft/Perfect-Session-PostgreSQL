5f39a1853ceeb46624547e13527869886aa3f667
Jonathan Guthrie <jono@perfect.org>
2017-01-30 11:24:30 -0500

    updated destroy

--- Sources/PostgresSessions.swift
+++ Sources/PostgresSessions.swift
@@ -74,9 +74,27 @@ public struct PostgresSessions {
 	}
 
 	/// Deletes the session for a session identifier.
-	public func destroy(token: String) {
+	public func destroy(_ request: HTTPRequest, _ response: HTTPResponse) {
 		let stmt = "DELETE FROM \(PostgresSessionConnector.table) WHERE token = $1"
-		exec(stmt, params: [token])
+		exec(stmt, params: [(request.session?.token)!])
+
+		// Reset cookie to make absolutely sure it does not get recreated in some circumstances.
+		var domain = ""
+		if !SessionConfig.cookieDomain.isEmpty {
+			domain = SessionConfig.cookieDomain
+		}
+		response.addCookie(HTTPCookie(
+			name: SessionConfig.name,
+			value: "",
+			domain: domain,
+			expires: .relativeSeconds(SessionConfig.idle),
+			path: SessionConfig.cookiePath,
+			secure: SessionConfig.cookieSecure,
+			httpOnly: SessionConfig.cookieHTTPOnly,
+			sameSite: SessionConfig.cookieSameSite
+			)
+		)
+
 	}
 
 	public func resume(token: String) -> PerfectSession {
