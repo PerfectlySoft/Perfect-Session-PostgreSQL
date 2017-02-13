3b1cfee807d05668f70b94726fbfac3ab5f4c1bf
Jonathan Guthrie <jono@perfect.org>
2017-02-13 12:07:51 -0500

    rename redirect

--- Sources/Redirect.swift
+++ Sources/Redirect.swift
@@ -22,7 +22,7 @@ import PerfectSession
 
 extension HTTPResponse {
 	/// Provides a convenience method for redirects
-	public func redirect(path: String, session: PerfectSession = PerfectSession()) {
+	public func sessionRedirect(path: String, session: PerfectSession = PerfectSession()) {
 
 		if !session.token.isEmpty  {
 			let driver = PostgresSessions()
