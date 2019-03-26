4a3394f1fcca53ede454604665da2f2139ab4fe5
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 13:44:57 -0400

    Updated package file

--- Package.swift
+++ Package.swift
@@ -1,11 +1,17 @@
+// swift-tools-version:4.2
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-01-12 20:41:34 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectSessionPostgreSQL",
-    targets: [],
+	products: [
+		.library(name: "PerfectSessionPostgreSQL", targets: ["PerfectSessionPostgreSQL"])
+	],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 3),
-    ]
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", from: "3.0.0"),
+    ],
+	targets: [
+		.target(name: "PerfectSessionPostgreSQL", dependencies: ["PerfectSession", "PerfectPostgreSQL"])
+	]
 )
